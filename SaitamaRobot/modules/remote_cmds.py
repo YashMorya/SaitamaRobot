78393e173b0f49620b90bc74284357cd1a46767b
Yash Morya <73687758+YashMorya@users.noreply.github.com>
2021-02-08 22:24:09 +0530

    Update remote_cmds.py

--- SaitamaRobot/modules/remote_cmds.py
+++ SaitamaRobot/modules/remote_cmds.py
@@ -459,7 +459,7 @@ RBAN_HANDLER = CommandHandler("rban", rban, filters=CustomFilters.sudo_filter)
 RUNBAN_HANDLER = CommandHandler(
     "runban", runban, filters=CustomFilters.sudo_filter)
 RKILL_HANDLER = CommandHandler(
-    "rkill", rkick, filters=CustomFilters.sudo_filter)
+    "rkill", rkill, filters=CustomFilters.sudo_filter)
 RMUTE_HANDLER = CommandHandler(
     "rmute", rmute, filters=CustomFilters.sudo_filter)
 RUNMUTE_HANDLER = CommandHandler(
